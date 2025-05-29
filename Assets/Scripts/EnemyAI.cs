41c7121ecfea9b0b6a05c4de204d1c838ffd99a6
Matyas Gulyas <142462528+M-G194@users.noreply.github.com>
2025-05-29 17:49:49 +1000

    Added enemy vision functionality
    - Enemy can now see the player. If an enemy sees the player, they will start pathing toward the last place they saw the player. If they have reached that place without seeing the player, they will return to patrolling.
    
    - Added
    - Added vision cone model
    - Added some collision layers in project settings and applied those layers to some prefabs (player, enemy, enemy vision cone, patrol nodes)
    - Added functionality to EnemyAI script

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,11 +7,18 @@ public class EnemyAI : MonoBehaviour
 {
     // The amount of time this enemy is stunned for
     private float stunTimer = 0.0f;
+    // If the player is in the enemy's vision
+    private bool canSeePlayer = false;
+
+    // A reference to the player object, populated when this enemy first sees the player
+    private GameObject playerObject;
 
     // Components
     private Pathfinding pathfinding;
     private FollowPatrolRoute followPatrolRoute;
     private NavMeshAgent navMeshAgent;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +28,18 @@ public class EnemyAI : MonoBehaviour
         followPatrolRoute = GetComponent<FollowPatrolRoute>();
         navMeshAgent = GetComponent<NavMeshAgent>();
 
+
         // Set the destination to the patrol route
         pathfinding.updateDestination(followPatrolRoute.destinationNode.transform.position);
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        // Decreasing the stun timer
         if (stunTimer > 0f)
         {
-            
             stunTimer -= Time.deltaTime;
             if (!navMeshAgent.isStopped)
                 navMeshAgent.isStopped = true;
@@ -40,14 +49,26 @@ public class EnemyAI : MonoBehaviour
             if (navMeshAgent.isStopped)
                 navMeshAgent.isStopped = false;
         }
-    }
 
+        // Path toward the player if they're in the enemy's vision
+        if (canSeePlayer)
+        {
+            pathfinding.updateDestination(playerObject.transform.position);
+        }
+
+        // If the NavMeshAgent isn't walking, continue patrolling
+        else if (navMeshAgent.velocity == new Vector3(0, 0, 0) && navMeshAgent.isStopped == false)
+        {
+            pathfinding.updateDestination(followPatrolRoute.destinationNode.transform.position);
+        }
+    }
 
 
     void OnCollisionEnter(Collision collision)
     {
         // Blank for now
     }
+
 
     void OnTriggerEnter (Collider collision) 
     {
@@ -58,8 +79,10 @@ public class EnemyAI : MonoBehaviour
         // Get the object that we collided with
         GameObject collidedObject = collision.gameObject;
 
-        // We check if the collision object has a PatrolNode before comparing it 
+        // Try to get components from the collided object to figure out what that object is
         PatrolNode node = collidedObject.GetComponent<PatrolNode>();
+        PlayerController player = collidedObject.GetComponent<PlayerController>();
+
         // If we collided with our destination patrol node
         if (node != null && node == followPatrolRoute.destinationNode)
         {
@@ -77,10 +100,40 @@ public class EnemyAI : MonoBehaviour
             {
                 Debug.LogError("Error! Enemy AI found no new destination node after colliding with a patrol node.");
             }
+
+            return;
+        }
+
+        // If our vision overlapped with a player
+        if (player != null)
+        {
+            // Save a reference to the player for later
+            if (playerObject == null){
+                playerObject = collidedObject;
+            }
+
+            canSeePlayer = true;
         }
         
     }
+
+
+    void OnTriggerExit (Collider collision)
+    {
+        // Get the object that we collided with
+        GameObject collidedObject = collision.gameObject;
+
+        // Try to get components off the collided object to figure out what that object is
+        PlayerController player = collidedObject.GetComponent<PlayerController>();
+
+        // If a player left our vision
+        if (player != null)
+        {
+            canSeePlayer = false;
+        }
+    }
     
+
     // Method to stun the enemy
     public void Stun(float duration)
     {
@@ -91,6 +144,7 @@ public class EnemyAI : MonoBehaviour
             Debug.Log($"Enemy stunned for {duration} seconds.");
         }
     }
+
 
     // Behaviour that runs when noise is received
     public void OnNoiseReceived(Vector3 noisePosition)
